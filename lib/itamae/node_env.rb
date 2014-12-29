d0688325de31312ec7eff294ce13df839f7bb083
nownabe <nownabe@gmail.com>
2014-12-30 01:02:58 +0900

    FIX to_h bug

--- lib/itamae/node_env.rb
+++ lib/itamae/node_env.rb
@@ -3,13 +3,25 @@ require "itamae/node_env/version"
 
 module Itamae
   module NodeEnv
-    def [](key)
-      val = super(key)
-      if val.is_a?(String) and /\Aenv\[([^\]]+)\]\z/ =~ val
-        ENV[$1]
-      else
-        val
+    def convert_env(val)
+      case val
+      when String then
+        if /\Aenv\[([^\]]+)\]\z/ =~ val
+          return ENV[$1]
+        end
+      when Array then
+        return val.map {|v| convert_env(v)}
+      when Hash then
+        hash = Hash.new
+        val.each {|k,v| hash[k] = convert_env(v)}
+        return hash
       end
+      return val
+    end
+
+    def initialize(source_hash = nil, default = nil, &blk)
+      source_hash = convert_env(source_hash)
+      super(source_hash, default, &blk)
     end
   end
 end
